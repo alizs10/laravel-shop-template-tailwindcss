6a8e834e72dfb89a80c19f2fc9f06bc23ff4ea78
Ali <ali.text77@gmail.com>
2022-05-31 23:43:59 +0430

    sidebar responsive

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,4 +1,33 @@
+var sidebarVisibility = false;
+var categories = $('#categories');
+var isOverCategories = false;
+
 function toggleSidebar() {
+    sidebarVisibility = !sidebarVisibility;
     $('aside').toggleClass('-right-full').toggleClass('right-0')
     $('#blur-back').toggleClass('hidden')
 }
+
+// $('#toggleCategories').on('mouseenter', function () {
+    
+    
+//     // check if screen is large
+//     if (!sidebarVisibility) {
+//         categories.removeClass('lg:hidden')
+//     }
+// });
+
+// // $('#toggleCategories').on('mouseleave', function () {
+// //     if (!isOverCategories && !sidebarVisibility) {
+// //         categories.addClass('lg:hidden')
+// //     }
+// // });
+
+
+// categories.on('mouseenter', function () {
+//     isOverCategories = true;
+// })
+// categories.on('mouseleave', function () {
+//     isOverCategories = false;
+//     categories.addClass('lg:hidden')
+// })
