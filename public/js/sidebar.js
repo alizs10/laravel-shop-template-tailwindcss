5e0934de754b193ab8fde91ce41085a9e2e2c518
Ali <ali.text77@gmail.com>
2022-06-01 18:17:23 +0430

    sidebar for big screens

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,33 +1,33 @@
-var sidebarVisibility = false;
 var categories = $('#categories');
-var isOverCategories = false;
-
 function toggleSidebar() {
-    sidebarVisibility = !sidebarVisibility;
     $('aside').toggleClass('-right-full').toggleClass('right-0')
     $('#blur-back').toggleClass('hidden')
 }
 
-// $('#toggleCategories').on('mouseenter', function () {
-    
-    
-//     // check if screen is large
-//     if (!sidebarVisibility) {
-//         categories.removeClass('lg:hidden')
-//     }
-// });
 
-// // $('#toggleCategories').on('mouseleave', function () {
-// //     if (!isOverCategories && !sidebarVisibility) {
-// //         categories.addClass('lg:hidden')
-// //     }
-// // });
+$('#toggleCategories').on('mouseenter', function () {
+    // check if screen is large
+
+    if (window.innerWidth > 1024) {
+        categories.addClass('lg:grid');
+        $('#blur-back').toggleClass('hidden lg:hidden')
+    }
+});
+
+$('#toggleCategories').on('mouseleave', function (event) {
+    var target = $(event.toElement)[0];
+    if (window.innerWidth > 1024 && categories.find(target).length == 0) {
+        categories.removeClass('lg:grid')
+        $('#blur-back').toggleClass('hidden lg:hidden')
+    }
+
+});
 
 
-// categories.on('mouseenter', function () {
-//     isOverCategories = true;
-// })
-// categories.on('mouseleave', function () {
-//     isOverCategories = false;
-//     categories.addClass('lg:hidden')
-// })
+
+categories.on('mouseleave', function () {
+    if (window.innerWidth > 1024) {
+        categories.removeClass('lg:grid')
+        $('#blur-back').toggleClass('hidden lg:hidden')
+    }
+})
