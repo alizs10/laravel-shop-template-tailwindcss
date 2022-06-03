3154ae3d96f84f58a3a3e71830e7d44b721af898
Ali <ali.text77@gmail.com>
2022-06-03 19:43:51 +0430

    add send vcode again link with timer

--- public/js/verification_code_input.js
+++ public/js/verification_code_input.js
@@ -1,8 +1,8 @@
 $('#verification_code_form input')[0].focus();
 
 $('#verification_code_form input').on('input', function (event) {
- 
-    if(isNaN($(this).val())) {
+
+    if (isNaN($(this).val())) {
         $(this).val('')
         return;
     }
@@ -24,3 +24,23 @@ $('#verification_code_form input').on('keydown', function (event) {
     }
 
 })
+
+
+var timer = 10;
+
+function countDown() {
+    const timerInterval = setInterval(function () {
+        timer -= 1;
+        $('#timer-text').html(`ارسال مجدد کد تایید در ${timer} ثانیه دیگر`)
+
+        if (timer <= 0) {
+            clearInterval(timerInterval)
+            var token = $('#token').val()
+            var email = $('#email').val()
+            $('#timer-text').html('<a class="text-xs text-red-500" href="/auth/send-verification-code' + `?email=${email}&token=${token}` +'">ارسال مجدد کد تایید</a>')
+        }
+    }, 1000)
+
+}
+
+countDown()
