3358f75ba55bc23ad95a0ee27c62909dbf506684
Ali Z.Soleimani <ali.text77@gmail.com>
2022-06-18 20:26:31 +0430

    update search page

--- public/js/search-page.js
+++ public/js/search-page.js
@@ -2,8 +2,16 @@ function toggleFilters() {
     $('body').toggleClass('overflow-hidden');
     $('#filters').toggleClass('hidden');
  }
+function togglePriceFilter(btn) { 
+   $(btn).parent().children().eq(1).toggleClass('hidden');
+
+ }
+function toggleCategoryFilter(btn) { 
+   $(btn).parent().children().eq(1).toggleClass('hidden');
+
+ }
 
  function toggleProductsExistFilter(btn)
  {
-    $(btn).parent().toggleClass('justify-end bg-gray-200 bg-red-500')
+    $(btn).parent().toggleClass('justify-end bg-gray-200 dark:bg-gray-700 bg-red-500')
  }
