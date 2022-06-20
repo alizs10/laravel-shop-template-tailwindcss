738cf7b2b83de2eea7f35379a5dbae0c58d63e1f
Ali Z.Soleimani <ali.text77@gmail.com>
2022-06-20 20:39:56 +0430

    add price filter

--- public/js/search-page.js
+++ public/js/search-page.js
@@ -30,3 +30,106 @@ function toggleSubCategoryFilter(cat_filter_id) {
 
 }
 
+// filter on price
+
+var rect = document.getElementById('price-filter-rect');
+var priceRange = document.getElementById('price-filter-range');
+var startPrice = document.getElementById('price-filter-start-btn');
+var endPrice = document.getElementById('price-filter-end-btn');
+var limitedWidth = startPrice.getBoundingClientRect().width + endPrice.getBoundingClientRect().width;
+
+endPrice.addEventListener('pointerdown', e => {
+   endPrice.setPointerCapture(e.pointerId)
+
+   setEndPriceRangePosition(e)
+   endPrice.addEventListener('pointermove', setEndPriceRangePosition)
+
+   document.addEventListener('pointerup', () => {
+
+      endPrice.removeEventListener('pointermove', setEndPriceRangePosition)
+   })
+})
+
+function setEndPriceRangePosition(e) {
+   let xEnd = e.clientX - rect.getBoundingClientRect().left;
+   console.log(e);
+
+   let endBtnPosition = e.clientX + endPrice.getBoundingClientRect().width;
+   let startBtnPosition = startPrice.getBoundingClientRect().x;
+   if (xEnd > 0) {
+
+      if (endBtnPosition < startBtnPosition) {
+         priceRange.style.left = `${xEnd}px`;
+         setEndPrice()
+      }
+
+   } else {
+      priceRange.style.left = `0`;
+   }
+}
+
+startPrice.addEventListener('pointerdown', e => {
+   startPrice.setPointerCapture(e.pointerId)
+   setStartPriceRangePosition(e)
+
+   startPrice.addEventListener('pointermove', setStartPriceRangePosition)
+
+   document.addEventListener('pointerup', () => {
+
+      startPrice.removeEventListener('pointermove', setStartPriceRangePosition)
+   })
+})
+
+function setStartPriceRangePosition(e) {
+   console.log(rect.getBoundingClientRect());
+   let xEnd = e.clientX - rect.getBoundingClientRect().left;
+   let movement = rect.getBoundingClientRect().width - xEnd;
+
+   let endBtnPosition = endPrice.getBoundingClientRect().x + endPrice.getBoundingClientRect().width;
+   let startBtnPosition = xEnd + rect.getBoundingClientRect().left - startPrice.getBoundingClientRect().width;
+
+
+   if (movement > 0) {
+
+      if (endBtnPosition < startBtnPosition) {
+         priceRange.style.right = `${movement}px`;
+         setStartPrice()
+      }
+
+
+   } else {
+      priceRange.style.right = `0`;
+   }
+
+
+}
+
+function e2pNumbers(number) {
+   let pNumber = priceFormatter(number);
+   let persianNumbers = ['۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۰']
+   let englishNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+   for (let i = 0; i < englishNumbers.length; i++) {
+      let req = new RegExp(englishNumbers[i], "g");
+      pNumber = pNumber.replace(req, persianNumbers[i])
+   }
+
+   return pNumber;
+}
+
+function priceFormatter(price) {
+   return parseInt(price).toLocaleString('en-US');
+}
+
+
+
+function setStartPrice() {
+   let ratio = 1 - ((priceRange.getBoundingClientRect().right - rect.getBoundingClientRect().left) / rect.getBoundingClientRect().width)
+   let price = $("input[name=end-price]").attr('max') * ratio.toFixed(2);
+   $("input[name=start-price]").val(e2pNumbers(price.toFixed(0)))
+}
+
+function setEndPrice() {
+   let ratio = 1 - ((priceRange.getBoundingClientRect().left - rect.getBoundingClientRect().left) / rect.getBoundingClientRect().width)
+   let price = $("input[name=end-price]").attr('max') * ratio.toFixed(2);
+   $("input[name=end-price]").val(e2pNumbers(price.toFixed(0)))
+}
--- public/js/test.js
+++ public/js/test.js
@@ -69,14 +69,32 @@ function setStartPriceRangePosition(e) {
 
 }
 
+function e2pNumbers(number) {
+   let pNumber = priceFormatter(number);
+   let persianNumbers = ['۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', '۰']
+   let englishNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+   for (let i = 0; i < englishNumbers.length; i++) {
+      let req = new RegExp(englishNumbers[i], "g");
+      pNumber = pNumber.replace(req, persianNumbers[i])
+   }
+  
+   return pNumber;
+}
+
+function priceFormatter(price) {
+   return parseInt(price).toLocaleString('en-US');
+}
+
+
+
 function setStartPrice() {
-   let x = 1 - priceRange.getBoundingClientRect().right / rect.getBoundingClientRect().width
-   let startPrice = $("input[name=end-price]").attr('max') * x;
-   $("input[name=start-price]").val(startPrice.toFixed(0))  
+   let ratio = 1 - priceRange.getBoundingClientRect().right / rect.getBoundingClientRect().width
+   let price = $("input[name=end-price]").attr('max') * ratio.toFixed(2);
+   $("input[name=start-price]").val(e2pNumbers(price.toFixed(0)))  
 }
 
 function setEndPrice() {
-   let x = 1 - priceRange.getBoundingClientRect().left / rect.getBoundingClientRect().width
-   let endPrice = $("input[name=end-price]").attr('max') * x;
-   $("input[name=end-price]").val(endPrice.toFixed(0))
+   let ratio = 1 - priceRange.getBoundingClientRect().left / rect.getBoundingClientRect().width
+   let price = $("input[name=end-price]").attr('max') * ratio.toFixed(2);
+   $("input[name=end-price]").val(e2pNumbers(price.toFixed(0)))
 }
